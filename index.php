7fc13bfbaf8d0e3664c42ef1491f1261405bc207
ChiaYEHHH <hedy821216@gmail.com>
2024-05-03 16:32:12 +0800

    背景漸層

--- index.php
+++ index.php
@@ -7,18 +7,47 @@
   <link rel="shortcut icon" href="#">
   <title>萬年曆作業</title>
   <style>
-   /*請在這裹撰寫你的CSS*/ 
+    body{
+      margin: 0;
+      padding: 0;
+      /* height: 100vh;
+      width: 100%; */
+      overflow: hidden;
+      background: linear-gradient(to right, rgb(189, 228, 167), rgb(122, 156, 198), #FFFD98);
+      animation: gradientAnimation 3s ease infinite alternate;
+    }
+    .bgc_gradient {
+      width: 100%;
+      height: 100%;
+      /* background: linear-gradient(to right, #ff7e5f, #feb47b, #ff7e5f);
+      animation: gradientAnimation 10s ease infinite alternate; */
+    }
+
+    @keyframes gradientAnimation {
+      0% {
+        background-position: 0% 50%;
+      }
+      25% {
+        background-position: 50% 100%;
+      }
+      50% {
+        background-position: 100% 50%;
+      }
+      100% {
+        background-position: 50% 100%;
+      }
+    }
     
   </style>
 </head>
 <body>
 <h1>萬年曆</h1>  
-
+<div class="bgc_gradient">
 <?php
 /*請在這裹撰寫你的萬年曆程式碼*/  
   
   
 ?>
-  
+  </div>
 </body>
 </html>
