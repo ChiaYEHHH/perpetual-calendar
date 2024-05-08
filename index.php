33ab0f5926baf2dd96cd078cb89a9abb24c8ff16
ChiaYEHHH <hedy821216@gmail.com>
2024-05-08 16:32:23 +0800

    背景debug

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,15 +8,15 @@
   <link rel="shortcut icon" href="#">
   <title>萬年曆作業</title>
   <style>
-    body{
+    body {
       margin: 0;
-      padding: 0;
-      /* height: 100vh;
-      width: 100%; */
-      overflow: hidden;
-      background: linear-gradient(to right, rgb(189, 228, 167), rgb(122, 156, 198), #FFFD98);
-      animation: gradientAnimation 3s ease infinite alternate;
+    padding: 0;
+    height: 100vh; /* 讓背景填滿整個畫面 */
+    animation: gradientAnimation 10s infinite; /* 使用名為 gradientAnimation 的動畫，持續 10 秒，無限循環 */
+    background: linear-gradient(to right, #ff5733, #ffbd69, #ffeea8); /* 初始漸層背景 */
+    background-size: 200% auto; /* 設定漸層大小為原來的兩倍寬 */
     }
+
     .bgc_gradient {
       width: 100%;
       height: 100%;
@@ -37,34 +38,47 @@
         background-position: 50% 100%;
       }
     }
-    .container{
+    .container {
       background-color: lightblue;
       width: 80%;
       height: 90vh;
-      margin:auto;
-      display:flex;
+      margin: auto;
+      display: flex;
       justify-content: space-around;
-      flex-wrap:wrap;
+      flex-wrap: wrap;
+
 
     }
-    .date{
+
+    .date {
       border: 5px double black;
       width: 12%;
       height: 8%;
+      margin: auto;
     }
   </style>
 </head>
+
 <body>
-<h1>萬年曆</h1>  
-<div class="bgc_gradient">
-  <div class="container">
-  <?php for ($i=1; $i <=48 ; $i++) : ?>
-    <!-- /*請在這裹撰寫你的萬年曆程式碼*/   -->
-    <!-- if($i) -->
-    
-    <div class="date"><?=$i;?></div>
-    
-    <?php endfor; ?>
-  </div>
-  </body>
+  <h1>萬年曆</h1>
+  <?php 
+$week=['日','一','二','三','四','五','六'];
+  ?>
+  <div class="bgc_gradient">
+    <div class="container">
+      <?php 
+      // foreach ($week as $w) {
+      //   <div> $i </div>
+      // }
+      ?>
+      <?php for ($i = 1; $i <= 49; $i++) : ?>
+        <!-- /*請在這裹撰寫你的萬年曆程式碼*/   -->
+        <!-- if($i) -->
+
+        <div class="date"><?= $i; ?></div>
+
+      <?php endfor; ?>
+    </div>
+</body>
+
 </html>
