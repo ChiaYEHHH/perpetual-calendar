c71d2a7171b6a5d110c43b2f08f8d9ef75e581c7
ChiaYEHHH <hedy821216@gmail.com>
2024-05-17 16:13:16 +0800

    魔改button onclick JS(未成功)

--- index.php
+++ index.php
@@ -14,15 +14,32 @@
 
 <body>
   <h1>Enjoy your days</h1>
-   
- <?php
+  
+  <?php
   // 寫出每個月的陣列
   $today = date("Y-m-d");
   $year = date("Y");
-  $mon = date("m");
+  $mon = 5;
   $firstDay = strtotime(date("$year-$mon-1"));
   $firstWeekStartDay = date("w", $firstDay);
   $days = date("t", $firstDay);
+  // 月份切換
+  $last_mon=$mon-1;
+  $next_mon=$mon+1;
+  if($next_mon > 12){
+    $newMon=1;
+    $newYear=$year+1;
+  }else{
+    $newMon=$next_mon;
+    $newYear=$year;
+  }
+  if($last_mon < 1){
+    $newMon=12;
+    $newYear=$year-1;
+  }else{
+    $newMon=$last_mon;
+    $newYear=$year;
+  }
   $month = [];
   for($i=1 ; $i <= 42 ; $i++){
     if ($i <= $firstWeekStartDay) {
@@ -36,32 +53,32 @@
     // echo "<pre>";
     // print_r($month);
     // echo "</pre>";
-}
-if($mon+1>12){
-  $mon=1;
-  $year=$year+1;
-}else{
-  $mon=$mon+1;
-}
-if($mon-1<1){
-  $mon=12;
-  $year=$year-1;
-}else{
-  $mon=$mon-1;
-}
+  }
 ?>
-  <div class="nav">
-    <div class="switch_lastmon">
-      <a href="">
-      <?php 
-      $mon=$mon-1; 
-      ?>
-      上一個月
-      </a>
+<script>
+  var mon = <?= $monChange; ?>
+  function changeMon(mon) {
+    // mon = last_mon;
+    return mon;
+  }
+  </script>
+<button onclick="changeMon(<?= $last_mon; ?>)">
+  上一個月
+</button>
+<button onclick="changeMon(<?= $next_mon; ?>)">
+  下一個月
+</button>
 
+<div class="nav">
+    <div class="switch_lastmon">
+      
+      
+      </div>
+    
+      <div class="premon"><?= date("F",$firstDay); ?></div>
+      
+      
     </div>
-    <div class="premon"><?= date("F",$firstDay); ?></div>
-    <div class="switch_nextmon"></div>
   </div>
   <?php
   $week = ['日', '一', '二', '三', '四', '五', '六'];
@@ -69,10 +86,11 @@ if($mon-1<1){
   <div class="bgc_gradient">
     <div class="calendar">
       <?php
+      // 周一至周五
       foreach ($week as $w) : ?>
         <div class="week"><?= $w; ?> </div>
       <?php endforeach; ?>
-
+        <!-- 日曆 -->
       <?php foreach ($month as $m) : ?>
         <div class="date">
           
