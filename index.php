b6aa74927297d6abdd0b4cbef3684e04390fcec5
ChiaYEHHH <hedy821216@gmail.com>
2024-05-19 01:28:10 +0800

    a連結php function測試失敗 div 調整

--- index.php
+++ index.php
@@ -14,8 +14,8 @@
 
 <body>
   <h1>Enjoy your days</h1>
-   
- <?php
+  <a href="test.php">測試頁</a>
+  <?php
   // 寫出每個月的陣列
   $today = date("Y-m-d");
   $year = date("Y");
@@ -23,8 +23,9 @@
   $firstDay = strtotime(date("$year-$mon-1"));
   $firstWeekStartDay = date("w", $firstDay);
   $days = date("t", $firstDay);
+  $week = ['日', '一', '二', '三', '四', '五', '六'];
   $month = [];
-  for($i=1 ; $i <= 42 ; $i++){
+  for ($i = 1; $i <= 42; $i++) {
     if ($i <= $firstWeekStartDay) {
       $month[] = 'QAQ';
     } elseif (($i - $firstWeekStartDay) <= $days) {
@@ -36,50 +37,40 @@
     // echo "<pre>";
     // print_r($month);
     // echo "</pre>";
-}
-if($mon+1>12){
-  $mon=1;
-  $year=$year+1;
-}else{
-  $mon=$mon+1;
-}
-if($mon-1<1){
-  $mon=12;
-  $year=$year-1;
-}else{
-  $mon=$mon-1;
-}
-?>
-  <div class="nav">
-    <div class="switch_lastmon">
-      <a href="">
-      <?php 
-      $mon=$mon-1; 
-      ?>
-      上一個月
-      </a>
+  }
+  if ($mon + 1 > 12) {
+    $mon = 1;
+    $year = $year + 1;
+  } else {
+    $mon = $mon + 1;
+  }
+  if ($mon - 1 < 1) {
+    $mon = 12;
+    $year = $year - 1;
+  } else {
+    $mon = $mon - 1;
+  }
+  ?>
+
+  <div class="calendar">
+
+    <div class="nav">
 
     </div>
-    <div class="premon"><?= date("F",$firstDay); ?></div>
-    <div class="switch_nextmon"></div>
-  </div>
-  <?php
-  $week = ['日', '一', '二', '三', '四', '五', '六'];
-  ?>
-  <div class="bgc_gradient">
-    <div class="calendar">
+    <!-- 月曆主體 -->
+    <div class="main">
       <?php
       foreach ($week as $w) : ?>
         <div class="week"><?= $w; ?> </div>
       <?php endforeach; ?>
-
       <?php foreach ($month as $m) : ?>
         <div class="date">
-          
+
           <?= $m; ?>
         </div>
       <?php endforeach; ?>
     </div>
+  </div>
 </body>
 
 </html>
---
+++ test.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+// $mon = isset($nextMon) ? $nextMon : date("m");
+$mon = isset($nextMon) ? $nextMon : date("m");
+
+$newMon=newmonth() ;
+function newmonth(){
+    global $mon;
+    return ($mon - 1) <= 0 ? 12 : $mon - 1;
+}
+?>
+<!-- a href="?lastMon=<?= $newMon ?>"> -->
+<a href="?lastMon=<?= $mon ?>">
+    上一個月
+</a>
+<br>
+<br>
+mon<?= $mon ?>
+
+</body>
+</html>
